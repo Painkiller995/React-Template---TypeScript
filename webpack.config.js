1eb5019f3430e6e8a7711f5cc4bd3f128aa7dd10
fahdd95 <fahdakramdaher@gmail.com>
2023-08-04 18:58:31 +0200

    initial

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const Modes = {
+  DEVELOPMENT: 'development',
+  PRODUCTION: 'production',
+};
+
+module.exports = (env, { mode }) => {
+  const isProduction = mode === Modes.PRODUCTION;
+
+  return {
+    mode,
+    entry: path.join(__dirname, 'src', 'index.tsx'),
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|jp(e*)g|gif|webp|avif)$/,
+          use: ['file-loader'],
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: 'svg-url-loader',
+              options: {
+                limit: 10000,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'public', 'index.html'),
+      }),
+    ],
+
+    performance: {
+      maxEntrypointSize: Infinity,
+      maxAssetSize: 1024 ** 2,
+    },
+
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+
+    devServer: {
+      host: 'localhost',
+      port: 3000,
+      historyApiFallback: true,
+    },
+  };
+};
